b5702567441bc3ed60e54db2f482c6b79fd763a7
azulsusana <35381762+azulsusana@users.noreply.github.com>
2020-02-18 19:57:50 -0800

    Initial Commit

--- UWPSoundBoard/MainPage.xaml.cs
+++ UWPSoundBoard/MainPage.xaml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using UWPSoundBoard.Model;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.Xaml;
@@ -22,14 +24,64 @@ namespace UWPSoundBoard
     /// </summary>
     public sealed partial class MainPage : Page
     {
+        private ObservableCollection<Sound> sounds;
+        private List<MenuItem> menuItems;
         public MainPage()
         {
             this.InitializeComponent();
+            sounds = new ObservableCollection<Sound>();
+            SoundManager.GetAllSounds(sounds);
+            menuItems = new List<MenuItem>();
+            //Load pane
+            menuItems.Add(new MenuItem
+            {
+                IconFile = "Assets/Icons/animals.png",
+                Category = SoundCategory.Animals
+            });
+            menuItems.Add(new MenuItem
+            {
+                IconFile = "Assets/Icons/cartoon.png",
+                Category = SoundCategory.Cartoons
+            });
+            menuItems.Add(new MenuItem
+            {
+                IconFile = "Assets/Icons/taunt.png",
+                Category = SoundCategory.Taunts
+            });
+            menuItems.Add(new MenuItem
+            {
+                IconFile = "Assets/Icons/warning.png",
+                Category = SoundCategory.Warnings
+            });
+
+
         }
 
         private void HamburgerButton_Click(object sender, RoutedEventArgs e)
         {
+            MySplitView.IsPaneOpen = !MySplitView.IsPaneOpen;
+        }
 
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            SoundManager.GetAllSounds(sounds);
+            CategoryTextBlock.Text = "All Sounds";
+            MenuItemsListView.SelectedItem = null;
+            BackButton.Visibility = Visibility.Collapsed;
+        }
+
+        private void SoundGridView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            var sound = (Sound)e.ClickedItem;
+            MyMediaElement.Source = new Uri(this.BaseUri, sound.AudioFile);
+        }
+
+        private void MenuItemsListView_ItemClick(object sender, ItemClickEventArgs e)
+        {
+            var menuItem = (MenuItem)e.ClickedItem;
+            CategoryTextBlock.Text = menuItem.Category.ToString();
+            SoundManager.GetSoundsByCategory(sounds, menuItem.Category);
+            BackButton.Visibility = Visibility.Visible;
         }
     }
 }
--- UWPSoundBoard/Model/SoundManager.cs
+++ UWPSoundBoard/Model/SoundManager.cs
@@ -1,12 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace UWPSoundBoard.Model
 {
-    class SoundManager
+    public static class SoundManager
     {
+        // Must be private for some reason she'll explain later
+        private static List<Sound> getSounds()
+        {
+            // Creating a list of sounds
+            // Variable declaration = instantiation
+            // List<Sound> sounds = new List<Sound>();
+
+            // Var infers type from right hand side
+            // Must be initialized 
+            var sounds = new List<Sound>();
+
+            sounds.Add(new Sound("Cow", SoundCategory.Animals));
+            sounds.Add(new Sound("Cat", SoundCategory.Animals));
+
+            sounds.Add(new Sound("Clock", SoundCategory.Taunts));
+            sounds.Add(new Sound("LOL", SoundCategory.Taunts));
+
+            sounds.Add(new Sound("Ship", SoundCategory.Warnings));
+            sounds.Add(new Sound("Siren", SoundCategory.Warnings));
+
+            sounds.Add(new Sound("Gun", SoundCategory.Cartoons));
+            sounds.Add(new Sound("Spring", SoundCategory.Cartoons));
+
+            return sounds;
+        }
+        public static void GetAllSounds(ObservableCollection<Sound> sounds)
+        {
+            var allSounds = getSounds();
+            sounds.Clear();
+
+            // Lambda
+            allSounds.ForEach(s => sounds.Add(s));
+        }
+        public static void GetSoundsByCategory(ObservableCollection<Sound> sounds, SoundCategory category)
+        {
+            var allSounds = getSounds();
+            var filteredSounds = allSounds.Where(s => s.Category == category).ToList();
+            sounds.Clear();
+            filteredSounds.ForEach(s => sounds.Add(s));
+        }
+
     }
 }
